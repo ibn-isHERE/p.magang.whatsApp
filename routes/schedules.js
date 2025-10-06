4a57efa767b226f22b6f9f58169c62737cf1a8f3
ibn-isHERE <ibnuraisaadibii@gmail.com>
2025-10-06 13:20:03 +0700

    update eror meeting

--- routes/schedules.js
+++ routes/schedules.js
@@ -11,7 +11,7 @@ let jobs = {}; // Untuk menyimpan job terjadwal
 let client = null;
 
 function setWhatsappClient(whatsappClient) {
-  client = whatsappClient;z
+  client = whatsappClient;
 }
 
 /**
